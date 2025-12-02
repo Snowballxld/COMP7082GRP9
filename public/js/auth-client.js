681fc5985f4904e12651367c066acc3fb6bb8b3b
AdamBcit <132965019+AdamBcit@users.noreply.github.com>
2025-12-01 21:50:16 -0800

    auth-client update

--- public/js/auth-client.js
+++ public/js/auth-client.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
 const firebaseConfig = window.firebaseConfig;
 const app = initializeApp(firebaseConfig);
